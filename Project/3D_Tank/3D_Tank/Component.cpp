ab8fa340e8b11ffa5179c3572cf72d0fd3391b8a
cuiyalei <cuiyalei2783@virtuos.com.cn>
2020-05-27 13:01:59 +0800

    update

--- Project/3D_Tank/3D_Tank/Component.cpp
+++ Project/3D_Tank/3D_Tank/Component.cpp
@@ -1,1 +1,11 @@
 #include "Component.h"
+
+bool Component::setObject(Object obj)
+{
+	return false;
+}
+
+Object Component::getObject()
+{
+	return Object();
+}
--- Project/3D_Tank/3D_Tank/EnginePlat.h
+++ Project/3D_Tank/3D_Tank/EnginePlat.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include <memory>
 #include "Windows.h"
@@ -16,13 +16,12 @@ namespace EngineRender
 
 #ifndef HR
 #define HR(x) { hr = x; if( FAILED(hr) ) { return hr; } } 
-#endif // !HR 
+#endif // !HR 自定义的HR宏，方便执行错误的返回
 
 #ifndef SAFE_DELETE
 #define SAFE_DELETE(p) { if(p) { delete(p); (p) = NULL; } }
-#endif // !SAFE_DELETE 
+#endif // !SAFE_DELETE 自定义的SAFE_DELETE()宏，便于指针释放
 
 #ifndef SAFE_RELEASE
 #define SAFE_RELEASE(p) { if(p) { (p)->Release(); (p) = NULL; } }
-#endif // !SAFE_RELEASE  
-
+#endif // !SAFE_RELEASE 自定义的SAFE_RELEASE()宏，便于COM资源的释放
