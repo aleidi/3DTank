e178efa8cc805c033f1565d7b37e0c5dbf56dae4
zhaotianhong <zhaotianhong2780@virtuos.com.cn>
2020-05-28 18:09:17 +0800

    +: add onUpdate
    +: add onInit
    +: add iskeyDown/iskeyUp/isMouseButtonDown/isMouseButtonUp

--- Project/3D_Tank/3D_Tank/DInput_PC.h
+++ Project/3D_Tank/3D_Tank/DInput_PC.h
@@ -12,11 +12,17 @@ public:
 	static DInputPC& getInstance();
 
 	////////////////////////////////////////////
-	HRESULT initialize(HWND hWnd, HINSTANCE hInstance, DWORD keyboardCoopFlags, DWORD mouseCoopFlags); // initialize DirectInput keyboard & mouse
-	void getInput();
-	bool iskeyDown(int iKey);
+	HRESULT onInit(HWND hWnd, HINSTANCE hInstance, DWORD keyboardCoopFlags, DWORD mouseCoopFlags); // initialize DirectInput keyboard & mouse
 
+	void onUpdate();
+	void getInput();
+	bool iskey(int iKey);
+	bool iskeyDown(int iKey);
+	bool iskeyUp(int iKey);
+
+	bool isMouseButton(int button);
 	bool isMouseButtonDown(int button);
+	bool isMouseButtonUp(int button);
 	float mouseDX();
 	float mouseDY();
 	float mouseDZ();
@@ -25,10 +31,11 @@ private:
 	IDirectInput8 *m_pDirectInput;
 	IDirectInputDevice8 * m_KeyboardDevice;
 	char m_keyBuffer[256];
-	char m_prekeyBuffer[256];
+	char pre_keyBuffer[256];
 
 	IDirectInputDevice8 * m_MouseDevice;
 	DIMOUSESTATE m_MouseState;
+	BYTE    pre_rgbButtons[4];
 
 	////////////////// singleton///////////////////////
 	DInputPC();
--- Project/3D_Tank/3D_Tank/Engine.cpp
+++ Project/3D_Tank/3D_Tank/Engine.cpp
@@ -25,7 +25,7 @@ Engine::Engine(Window& wnd)
 
 	mGraphics->SetProjection(DirectX::XMMatrixPerspectiveLH(1.0f, 3.0f / 4.0f, 0.5f, 40.0f));
 
-	DInputPC::getInstance().initialize(wnd.getHwnd(),wnd.getHinst(), DISCL_FOREGROUND | DISCL_NONEXCLUSIVE, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE );
+	DInputPC::getInstance().onInit(wnd.getHwnd(),wnd.getHinst(), DISCL_FOREGROUND | DISCL_NONEXCLUSIVE, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE );
 
 }
 
@@ -36,7 +36,7 @@ void Engine::run()
 	// physics.Update()
 	// input.update();
 	// input test code
-	DInputPC::getInstance().getInput();
+	DInputPC::getInstance().onUpdate();
 	// if (DInputPC::getInstance().iskeyDown(DIK_A)) MessageBox(mWnd.getHwnd(), L"I pressed A", L"Input_Test", MB_OK);
 
 	/*
@@ -55,25 +55,26 @@ void Engine::run()
 			c->Update(mTimer.getDeltaTIme());
 			
 			////////////////Rotate///////////////////
-			if (DInputPC::getInstance().isMouseButtonDown(0)) {
+			/*
+			if (DInputPC::getInstance().isMouseButton(0)) {
 				c->Rotate(DInputPC::getInstance().mouseDY()*mTimer.getDeltaTIme() * 50, DInputPC::getInstance().mouseDX()*mTimer.getDeltaTIme() * 50,
 					DInputPC::getInstance().mouseDZ()*mTimer.getDeltaTIme() * 50);
 			}
-			 
+			*/
 			///////////////Translate//////////////////
-			if (DInputPC::getInstance().iskeyDown(DIK_A)) fTrans_x = -0.05f;
-			else if (DInputPC::getInstance().iskeyDown(DIK_D)) fTrans_x = 0.05f;
-			else fTrans_x = 0.0f;
-			if (DInputPC::getInstance().iskeyDown(DIK_SPACE)) fTrans_y = 0.05f;
-			else if (DInputPC::getInstance().iskeyDown(DIK_LCONTROL)) fTrans_y = -0.05f;
+			if (DInputPC::getInstance().isMouseButtonDown(0)) fTrans_y = 5.0f;
+			else if (DInputPC::getInstance().isMouseButtonUp(1)) fTrans_y = -5.0f;
 			else fTrans_y = 0.0f;
-			if (DInputPC::getInstance().iskeyDown(DIK_W)) fTrans_z = 0.05f;
-			else if (DInputPC::getInstance().iskeyDown(DIK_S)) fTrans_z = -0.05f;
+			if (DInputPC::getInstance().iskey(DIK_W)) fTrans_z = 0.05f;
+			else if (DInputPC::getInstance().iskey(DIK_S)) fTrans_z = -0.05f;
 			else fTrans_z = 0.0f;
+			if (DInputPC::getInstance().iskeyDown(DIK_A)) fTrans_x = -5.0f;
+			else if (DInputPC::getInstance().iskeyUp(DIK_D)) fTrans_x = 5.0f;
+			else fTrans_x = 0.0f;
 			c->Translate(fTrans_x, fTrans_y, fTrans_z);
-			
 			/////////////////Scale////////////////////
-			if (DInputPC::getInstance().isMouseButtonDown(1)) {
+			/*
+			if (DInputPC::getInstance().isMouseButton(1)) {
 				if (DInputPC::getInstance().mouseDZ() > 0) {
 					fScale += 0.05f ;
 					c->Scale(fScale, fScale, fScale);
@@ -83,20 +84,8 @@ void Engine::run()
 					c->Scale(fScale, fScale, fScale);
 				}
 			}
-			
-			/*
-			if (DInputPC::getInstance().isMouseButtonDown(1)) {
-				std::wostringstream outss;
-				outss.precision(6);
-				if (DInputPC::getInstance().mouseDZ() != 0) {
-					i = i*DInputPC::getInstance().mouseDZ()*0.01;
-					i++;
-					outss << WNDTITLE << "i:" << i;
-					MessageBoxW(mWnd.getHwnd(), outss.str().c_str(), L"Input_Test", MB_OK);
-				}
-			} */
-			
-			c->Draw(*mGraphics);
+			*/
+			c->Draw(*mGraphics); 
 		}
 	}
 	mGraphics->EndFrame();
@@ -120,7 +109,7 @@ void Engine::calculateFrameStats()
 
 		std::wostringstream outs;
 		outs.precision(6);
-		outs << WNDTITLE << "   " << "FPS: " << fps << "   " << "Frame Time: " << mspf << "(ms)" ;
+		outs << WNDTITLE << "   " << "FPS: " << fps << "   " << "Frame Time: " << mspf << "(ms)";
 		SetWindowText(mWnd.getHwnd(), outs.str().c_str());
 
 		frameCnt = 0;
