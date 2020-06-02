559d02128cb0f2cd5d188c3cd0a09334a0a80587
zhaotianhong <zhaotianhong2780@virtuos.com.cn>
2020-06-02 18:53:28 +0800

    +: add 3D_Tank.vcxproj & 3D_Tank.vcxproj.filters ( unfinished EnemyTank class )

--- Project/3D_Tank/3D_Tank/EnemyTank.cpp
+++ Project/3D_Tank/3D_Tank/EnemyTank.cpp
@@ -1,12 +1,12 @@
 #include "EnemyTank.h"
-
+/*
 EnemyTank::EnemyTank(int id) : m_HP(0),
                                BaseGameEntity(id)
 {
 	m_pStateMachine = new StateMachine<EnemyTank>(this);
 	// m_pStateMachine->setCurrentState( A_STATE::Instance() );
 	// m_pStateMachine->setGlobalState( EnemyTankGlobalState::Instance() );
-}
+}*/
 
 EnemyTank::~EnemyTank() { delete m_pStateMachine; }
 							  
