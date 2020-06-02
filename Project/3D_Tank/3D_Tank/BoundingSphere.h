ba5ca6652c902bb0e77f32976ccfd8f7922803f1
cuiyalei <cuiyalei2783@virtuos.com.cn>
2020-06-02 18:33:35 +0800

    add function in BoundingSphere

--- Project/3D_Tank/3D_Tank/BoundingSphere.h
+++ Project/3D_Tank/3D_Tank/BoundingSphere.h
@@ -2,16 +2,20 @@
 #include "DirectXCollision.h"
 #include "Vector3.h"
 
-class BoundingSphere {
-public:
-	BoundingSphere();
-	BoundingSphere(const Vector3&, const float&);
-	BoundingSphere(const BoundingSphere&);
-	~BoundingSphere();
+class Ray;
 
-	void transform(BoundingSphere&, const float&, const Vector3&, const Vector3&);
+class MBoundingSphere {
+public:
+	MBoundingSphere();
+	MBoundingSphere(const Vector3&, const float&);
+	MBoundingSphere(const MBoundingSphere&);
+	~MBoundingSphere();
+
+	void transform(MBoundingSphere&, const float&, const Vector3&, const Vector3&);
 	Vector3 getCenter();
 	DirectX::BoundingSphere getSphere();
+	bool Collision(const MBoundingSphere&);
+	bool isIntersectRay(Ray*, float&);
 
 private:
 	DirectX::BoundingSphere* gameSphere;
