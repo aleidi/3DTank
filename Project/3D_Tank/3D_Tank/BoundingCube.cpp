ac0d9c4f7344b3310f7d3f342de53f1fcba401af
cuiyalei <cuiyalei2783@virtuos.com.cn>
2020-06-02 10:20:27 +0800

    update class name BoundingVolume to BoundingCube

--- Project/3D_Tank/3D_Tank/BoundingCube.cpp
+++ Project/3D_Tank/3D_Tank/BoundingCube.cpp
@@ -1,6 +1,6 @@
-#include "BoundingVolume.h"
+#include "BoundingCube.h"
 
-BoundingVolume::BoundingVolume(const Vector3& center, const Vector3& extents) {
+BoundingCube::BoundingCube(const Vector3& center, const Vector3& extents) {
 	DirectX::XMFLOAT3 cen{ center.x,center.y,center.z };
 	DirectX::XMFLOAT3 ext{ extents.x, extents.y, extents.z };
 	if (AABB == NULL) {
@@ -12,25 +12,25 @@ BoundingVolume::BoundingVolume(const Vector3& center, const Vector3& extents) {
 	}
 }
 
-BoundingVolume::BoundingVolume(const BoundingVolume& bv) :AABB(bv.AABB) {}
+BoundingCube::BoundingCube(const BoundingCube& bv) :AABB(bv.AABB) {}
 
-BoundingVolume::~BoundingVolume() {
+BoundingCube::~BoundingCube() {
 	if (AABB != NULL)
 		delete AABB;
 	AABB = NULL;
 }
 
-void BoundingVolume::createAABB(BoundingVolume& out, const Vector3& pt1, const Vector3& pt2) {
+void BoundingCube::createAABB(BoundingCube& out, const Vector3& pt1, const Vector3& pt2) {
 	DirectX::FXMVECTOR position1{ pt1.x,pt1.y,pt1.z };
 	DirectX::FXMVECTOR position2{ pt2.x,pt2.y,pt2.z };
 	DirectX::BoundingBox::CreateFromPoints(*out.AABB, position1, position2);
 }
 
-bool BoundingVolume::isCollision(const BoundingVolume& bv1, const BoundingVolume& bv2) {
+bool BoundingCube::isCollision(const BoundingCube& bv1, const BoundingCube& bv2) {
 	return bv1.AABB->Intersects(*bv2.AABB);
 }
 
-bool BoundingVolume::isRayIntersectsBV(const BoundingVolume& bv, Ray& ray, float& dis) {
+bool BoundingCube::isRayIntersectsBV(const BoundingCube& bv, Ray& ray, float& dis) {
 	DirectX::FXMVECTOR origin{ ray.getOrigin().x,ray.getOrigin().y,ray.getOrigin().z };
 	DirectX::FXMVECTOR direction{ ray.getDirection().x,ray.getDirection().y,ray.getDirection().z };
 	return AABB->Intersects(origin, direction, dis);
