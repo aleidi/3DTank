bb94bfb852d1a64be903818b5bd24813b72080b2
cuiyalei <cuiyalei2783@virtuos.com.cn>
2020-06-15 10:19:21 +0800

    update function to get model Vmax and Vmin

--- Project/3D_Tank/3D_Tank/FileManager.cpp
+++ Project/3D_Tank/3D_Tank/FileManager.cpp
@@ -30,6 +30,50 @@ void FileManager::LoadOBJModel(GeometryGenerator::Mesh & mesh, const std::string
 	}
 }
 
+void FileManager::LoadOBJModel(GeometryGenerator::Mesh & mesh, const std::string & name, DirectX::XMVECTOR & maxPoint, DirectX::XMVECTOR & minPoint)
+{
+	char input;
+	int flag = 0;
+	GeometryGenerator::Vertex vertex;
+	UINT index = 0;
+	std::fstream fin;
+	std::string path;
+	path = ".\\Resource\\Model\\" + name + ".txt";
+	fin.open(path);
+
+	if (!fin.good())
+	{
+		return;
+	}
+
+	//fin.get(input);
+	DirectX::XMFLOAT3 max, min, temp;
+	DirectX::XMVECTOR tempP;
+	while (!fin.eof())
+	{
+		fin >> vertex.Position.x >> vertex.Position.y >> vertex.Position.z >> vertex.Texcoord.x >> vertex.Texcoord.y;
+		if (flag == 0) {
+			max.x = vertex.Position.x; max.y = vertex.Position.y; max.z = vertex.Position.z;
+			min.x = vertex.Position.x; min.y = vertex.Position.y; min.z = vertex.Position.z;
+			maxPoint = DirectX::XMLoadFloat3(&max); minPoint = DirectX::XMLoadFloat3(&min);
+			flag++;
+		}
+		else {
+			temp.x = vertex.Position.x; temp.y = vertex.Position.y; temp.z = vertex.Position.z;
+			tempP = DirectX::XMLoadFloat3(&temp);
+			maxPoint = DirectX::XMVectorMax(maxPoint, tempP);
+			minPoint = DirectX::XMVectorMin(minPoint, tempP);
+		}
+		fin.get(input);
+		while (input != '\n')
+		{
+			fin.get(input);
+		}
+		mesh.vertices.push_back(vertex);
+		mesh.indices.push_back(index++);
+	}
+}
+
 wchar_t * FileManager::GetTexture(const std::string & name)
 {
 	std::string path;
--- Project/3D_Tank/3D_Tank/ModelMesh.cpp
+++ Project/3D_Tank/3D_Tank/ModelMesh.cpp
@@ -33,6 +33,38 @@ ModelMesh::ModelMesh(RenderComponent * owner, const std::string vertex, const st
 	AddBind(std::make_unique<TransformCbuf>(gfx, *this));
 }
 
+ModelMesh::ModelMesh(RenderComponent * owner, const std::string vertex, const std::wstring & texture, DirectX::XMVECTOR & maxPoint, DirectX::XMVECTOR & minPoint)
+	:Mesh(owner)
+{
+	Graphics& gfx = RenderManager::sGetInstance()->getGraphics();
+	GeometryGenerator::Mesh mesh;
+	GeometryGenerator::getModel(mesh, vertex, maxPoint, minPoint);
+	AddBind(std::make_unique<VertexBuffer>(gfx, mesh.vertices));
+
+	auto pvs = std::make_unique<VertexShader>(gfx, L"VertexShaderTex.cso");
+	auto pvsbc = pvs->GetBytecode();
+	AddBind(std::move(pvs));
+
+	AddBind(std::make_unique<PixelShader>(gfx, L"PixelShaderTex.cso"));
+
+	AddIndexBuffer(std::make_unique<IndexBuffer>(gfx, mesh.indices));
+
+	const std::vector<D3D11_INPUT_ELEMENT_DESC> ied =
+	{
+		{ "POSITION",0,DXGI_FORMAT_R32G32B32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
+		{ "TEXCOORD",0,DXGI_FORMAT_R32G32_FLOAT,0,12,D3D11_INPUT_PER_VERTEX_DATA,0},
+	};
+	AddBind(std::make_unique<InputLayout>(gfx, ied, pvsbc));
+
+	AddBind(std::make_unique<Texture>(gfx, texture.c_str()));
+
+	AddBind(std::make_unique<Sampler>(gfx));
+
+	AddBind(std::make_unique<Topology>(gfx, D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
+
+	AddBind(std::make_unique<TransformCbuf>(gfx, *this));
+}
+
 ModelMesh::ModelMesh(RenderComponent * owner, const std::string vertex, const std::wstring & texture, D3D11_SAMPLER_DESC sampDesc)
 	:Mesh(owner)
 {
