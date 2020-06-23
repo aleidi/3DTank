8e94adc8b46a8f24543d4e415321d213b5ec09de
zhaotianhong <zhaotianhong2780@virtuos.com.cn>
2020-06-23 20:12:08 +0800

    there are some problems ( ask cyl tomorrow

--- Project/3D_Tank/3D_Tank/EnemyTank.cpp
+++ Project/3D_Tank/3D_Tank/EnemyTank.cpp
@@ -108,11 +108,12 @@ bool EnemyTank::isEnemyInRange()const {
 	if( Vector3::lengthSq( getPlayerPos, mTransform->getPosition() ) <= mAttribute.m_AttackRangeRadiusSq ) {
 		//check if there are any obstacles
 		float distance = 0.0f;
-		bool isObstacle = CollisionManager::sGetInstance()->rayCheckWithObstacle(mTransform->getPosition(),
-															   (getPlayerPos - mTransform->getPosition()).normalize(),
-															   sqrt(Vector3::lengthSq(getPlayerPos, mTransform->getPosition())),
-															   nullptr, distance);
-		if( !isObstacle ) return true;
+		//bool isObstacle = CollisionManager::sGetInstance()->rayCheckWithObstacle(mTransform->getPosition(),
+			//												   (getPlayerPos - mTransform->getPosition()).normalize(),
+			//												   sqrt(Vector3::lengthSq(getPlayerPos, mTransform->getPosition())),
+			//												   nullptr, distance);
+		if(0)//if( !isObstacle ) 
+			return true;
 		else return false;
 		// else return false;return true; 
 	}
