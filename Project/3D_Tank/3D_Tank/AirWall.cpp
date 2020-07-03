a203131babcc1547eecae4db08b81c24a9e27eac
zhaoshengchao <zhaoshengchao2778@virtuos.com>
2020-07-03 13:48:28 +0800

    don't show AirWall

--- Project/3D_Tank/3D_Tank/AirWall.cpp
+++ Project/3D_Tank/3D_Tank/AirWall.cpp
@@ -3,19 +3,25 @@
 
 AirWall::AirWall(const Vector3& position, const Vector3& scale)
 {
-	cube = SceneManager::sGetInstance()->createCube();
-	cube->getTransform()->setPosition(position);
-	cube->getTransform()->setScale(scale);
-	cube->attach(*this);
-	cube->setName("airwallcube");
-	mCollisionBox = new BoundingCube(cube);
+	//cube = SceneManager::sGetInstance()->createCube();
+	//cube->getTransform()->setPosition(position);
+	//cube->getTransform()->setScale(scale);
+	//cube->attach(*this);
+	//cube->setName("airwallcube");
+	
+	mTransform->setPosition(position);
+	mTransform->setScale(scale);
+	mName = "airwall";
+
+	mCollisionBox = new BoundingCube(this);
 	mCollisionBox->createBoundingCube(position, Vector3(0.5 * scale.x, 0.5 * scale.y, 0.5 * scale.z), 0);
-	cube->addComponent(mCollisionBox);
-	cube->cube = mCollisionBox;
-	//mName = "airwall";
+	addComponent(mCollisionBox);
+	cube = mCollisionBox;
+	//cube->addComponent(mCollisionBox);
+	//cube->cube = mCollisionBox;
 }
 
 AirWall::~AirWall()
 {
-	cube->destroy();
+	//cube->destroy();
 }
