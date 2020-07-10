ddb0b1b8080fc89ba8246ac269627def84a83906
zhengzhiyuan <zhengzhiyuan2781@virtuos.com.cn>
2020-07-10 11:32:20 +0800

    adjust imGuiFrame structure

--- Project/3D_Tank/3D_Tank/ImGuiFrame.h
+++ Project/3D_Tank/3D_Tank/ImGuiFrame.h
@@ -8,6 +8,13 @@
 class ImGuiFrame
 {
 public:
+	ImGuiFrame(HWND hwnd, ID3D11Device* device, ID3D11DeviceContext* context);
+	~ImGuiFrame();
+
+	void onInit();
+	void onUpdate(float deltaTime);
+
+private:
 	ImGuiWindowFlags window_flags = 0;
 	std::map<std::string, bool> selectKey;
 	bool isNewAITank = false;
@@ -19,12 +26,6 @@ public:
 	bool isBindChanged = false;
 	bool isChangeToNight = false;
 	bool isChangeToSand = false;
-
-	ImGuiFrame(HWND hwnd, ID3D11Device* device, ID3D11DeviceContext* context);
-	~ImGuiFrame();
-
-	void onInit();
-	void onUpdate(float deltaTime);
 
 	void selectObject(std::string objectName);
 	void newObject(std::string gameObjectName);
